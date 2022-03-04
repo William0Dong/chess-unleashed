9565328604c1cf6134bb00d31b83100a3f99d972
William0Dong <chuanhao.tung@gmail.com>
2022-03-03 21:15:29 -0500

    Faster loading with JQuery CDN and better zoom friendliness for homepage Modified index.html such that there are no useless wrappers. Made it so header takes up the whole width, while bottom two images are not dynamically sized and instead static. Button text is now visible when zoomed in using scrollbar. Animations are stopped at a certain zoom point and then resumed when window width is not below 900px. Gradient between two chess player images now stays consistent instead of one fading too much onto the other. Rendered chessboard properly in custom-chess.html. Header becomes vertically oriented for zoom friendliness below 900px. Used CDN for CSS of chessboard for reduced file load request from local server.

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,73 @@
-setTimeout(function() {
-    document.getElementById('button-one').className = "postload";
-    document.getElementById('button-two').className = "postload";
-    document.getElementById('gif-one').className = "postload gif";
-    document.getElementById('gif-two').className = "postload gif";
+setTimeout(function () {
+  let button_one = document.getElementById("button-one");
+  let gif_one = document.getElementById("gif-one");
+  let button_two = document.getElementById("button-two");
+  let gif_two = document.getElementById("gif-two");
+  if ($(window).width() <= 900) {
+    button_one.classList.remove("preload", "postload", "animated");
+    button_two.classList.remove("preload", "postload", "animated");
+    gif_one.classList.remove("preload", "postload", "animated");
+    gif_two.classList.remove("preload", "postload", "animated");
+  }
+}, 0)
+
+setTimeout(function () {
+  if ($(window).width() > 900) {
+    document.getElementById("button-one").className = "postload";
+    document.getElementById("button-two").className = "postload";
+    document.getElementById("gif-one").className = "postload gif";
+    document.getElementById("gif-two").className = "postload gif";
+  }
 }, 1000);
 
-setTimeout(function() {
-    let button = document.getElementById('button-one');
-    let gif = document.getElementById('gif-one');
-    console.log(button);
+setTimeout(mouseoverOne, 1000);
 
-    function addAnim() {
-        button.classList.add('animated');
-        gif.classList.add('animated');
-        button.removeEventListener('mouseover', addAnim);
-    };
+setTimeout(mouseoverTwo, 1000);
 
-    button.addEventListener('mouseover', addAnim);
-}, 1000);
+function mouseoverOne() {
+  let button = document.getElementById("button-one");
+  let gif = document.getElementById("gif-one");
+  console.log(button);
 
-setTimeout(function() {
-    let button = document.getElementById('button-two');
-    let gif = document.getElementById('gif-two');
-    console.log(button);
+  function addAnim() {
+    if ($(window).width() > 900) {
+      button.classList.add("animated");
+      gif.classList.add("animated");
+    }
+  }
 
-    function addAnim() {
-        button.classList.add('animated');
-        gif.classList.add('animated');
-        button.removeEventListener('mouseover', addAnim);
-    };
+  button.addEventListener("mouseover", addAnim);
+}
 
-    button.addEventListener('mouseover', addAnim);
-}, 1000);
+function mouseoverTwo() {
+  let button = document.getElementById("button-two");
+  let gif = document.getElementById("gif-two");
+  console.log(button);
+
+  function addAnim() {
+    if ($(window).width() > 900) {
+      button.classList.add("animated");
+      gif.classList.add("animated");
+    }
+  }
+
+  button.addEventListener("mouseover", addAnim);
+}
+
+$(window).resize(function () {
+  let button_one = document.getElementById("button-one");
+  let gif_one = document.getElementById("gif-one");
+  let button_two = document.getElementById("button-two");
+  let gif_two = document.getElementById("gif-two");
+  if ($(window).width() <= 900) {
+    button_one.classList.remove("preload", "postload", "animated");
+    button_two.classList.remove("preload", "postload", "animated");
+    gif_one.classList.remove("preload", "postload", "animated");
+    gif_two.classList.remove("preload", "postload", "animated");
+  } else {
+    button_one.classList.add("postload");
+    button_two.classList.add("postload");
+    gif_one.classList.add("postload");
+    gif_two.classList.add("postload");
+  }
+});
