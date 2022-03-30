b0bce035d95160a84e9b93aac794efdc1eef95bd
William0Dong <chuanhao.tung@gmail.com>
2022-03-30 17:30:44 -0400

    Simplified back-end modules

--- backend/save-load.js
+++ backend/save-load.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const port = 3000
 const mysql = require('mysql2')
-const http = require('http')
 
 
 app.use(cors())
-app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res)=>{
